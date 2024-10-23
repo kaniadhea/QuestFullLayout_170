5193e9e0b3e980c4a786de02aad2a8bdb28eec33
kaniadhea <kaniadhea7@gmail.com>
2024-10-23 11:42:55 +0700

    aa

---
+++ app/src/main/java/com/example/prakti2/Layout.kt
@@ -0,0 +1,93 @@
+package com.example.prakti2
+
+import android.text.Layout
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview (showBackground = true )
+@Composable
+fun Layout(modifier: Modifier = Modifier) {
+    Column (modifier = modifier.fillMaxSize()) {
+        TampilanHeader()
+        DetailMahasiswa(
+            judul = "Nama",
+            isinya = "Dhea kania"
+        )
+        DetailMahasiswa(
+            judul = "NIM",
+            isinya = "20220140170"
+        )
+
+    }
+}
+
+@Composable
+fun TampilanHeader() {
+    Box(modifier = Modifier.fillMaxSize()
+        .background(Color.DarkGray)) {
+        Row {
+            Box(
+                contentAlignment = Alignment.BottomEnd
+            ) {
+                Image(
+                    painterResource(R.drawable.umy),
+                    contentDescription = null)
+
+                Icon(Icons.Default.Done,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(28.dp)
+                        .background(color = Color.Red, shape = CircleShape),
+                    tint = Color.DarkGray
+                )
+            }
+            Column ( modifier = Modifier.padding(16.dp)) {
+                Text(text = "Teknologi Informasi",
+                    color = Color.White,
+                    fontSize = 20.sp)
+                Spacer(modifier = Modifier.padding(3.dp))
+                Text(text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.White,
+                    fontSize = 17.sp)
+            }
+
+        }
+    }
+}
+
+@Composable
+fun DetailMahasiswa(
+    judul : String,
+    isinya : String
+) {
+    Row (modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween){
+        Text(text = judul  , modifier = Modifier.weight(0.8f) )
+        Text(text = ":", modifier = Modifier.weight(0.2f))
+        Text(text = isinya,
+            modifier = Modifier.weight(2f))
+    }
+}
---
+++ app/src/main/java/com/example/prakti2/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.prakti2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.prakti2.ui.theme.Prakti2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Prakti2Theme {
+                Scaffold (modifier = Modifier.fillMaxSize()) {innerPadding ->
+                 Layout(modifier = Modifier.padding(innerPadding))
+                }
+
+            }
+        }
+    }
+}
+
